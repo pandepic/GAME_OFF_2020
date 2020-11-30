308d5802c2a80e92559b0966a8f54c1a881ae9b5
pandepic <39251913+pandepic@users.noreply.github.com>
2020-11-30 19:39:36 +1100

    infinite scrolling star background

--- GameStates/GameStatePlay.cs
+++ GameStates/GameStatePlay.cs
@@ -10,13 +10,17 @@ namespace GAME_OFF_2020.GameStates
     public class GameStatePlay : GameState
     {
         public const float CHARACTER_SPEED = 100f;
+        public const float BACKGROUND_SPEED = 15f;
         public const float CHARACTER_Y = 217;
 
         public Character Player => CharacterManager.Player;
 
+        public Random RNG = new Random();
         public Camera2D Camera;
+        public Camera2D BackgroundCamera;
         public TiledMap Map;
         public TiledMapRenderer MapRenderer;
+        public TileBatch2D BackgroundStars;
         public CharacterManager CharacterManager;
 
         public SpriteBatch2D SpriteBatch;
@@ -29,9 +33,34 @@ namespace GAME_OFF_2020.GameStates
 
             Map = AssetManager.LoadTiledMap("Ship.tmx");
             MapRenderer = new TiledMapRenderer(Map, null, AssetManager.LoadTexture2D("SubwayShip.png"));
-            Camera = new Camera2D(new Rectangle(0, 0, ElementGlobals.TargetResolutionWidth, ElementGlobals.TargetResolutionHeight));
-            Camera.BoundingBox = new Rectangle(0, -5000, Map.MapSize.X * Map.TileSize.X, 10000);
-            Camera.Zoom = 2;
+            Camera = new Camera2D(new Rectangle(0, 0, ElementGlobals.TargetResolutionWidth, ElementGlobals.TargetResolutionHeight))
+            {
+                BoundingBox = new Rectangle(0, -5000, Map.MapSize.X * Map.TileSize.X, 10000),
+                Zoom = 2
+            };
+
+            BackgroundCamera = new Camera2D(new Rectangle(0, 0, ElementGlobals.TargetResolutionWidth, ElementGlobals.TargetResolutionHeight))
+            {
+                Zoom = 2
+            };
+
+            BackgroundStars = new TileBatch2D(4000, 200, Map.TileSize.X, Map.TileSize.Y, AssetManager.LoadTexture2D("SubwayShip.png"), TileBatch2DWrapMode.Both);
+            BackgroundStars.BeginBuild();
+            for (var y = 0; y < BackgroundStars.MapHeight; y++)
+            {
+                for (var x = 0; x < BackgroundStars.MapWidth; x++)
+                {
+                    var rng = RNG.Next(0, 100);
+
+                    if (rng > 2)
+                        BackgroundStars.SetTileAtPosition(x, y, 16);
+                    else if (rng == 1)
+                        BackgroundStars.SetTileAtPosition(x, y, 32);
+                    else
+                        BackgroundStars.SetTileAtPosition(x, y, 48);
+                }
+            }
+            BackgroundStars.EndBuild();
 
             CharacterManager = new CharacterManager("Crew.json");
         }
@@ -76,6 +105,9 @@ namespace GAME_OFF_2020.GameStates
 
         public override void Update(GameTimer gameTimer)
         {
+            BackgroundCamera.X += BACKGROUND_SPEED * gameTimer.DeltaS;
+            BackgroundCamera.Update(gameTimer);
+
             CharacterManager.Update(gameTimer, Map);
             Camera.Center(Player.Position.ToVector2I());
             Camera.Update(gameTimer);
@@ -83,6 +115,7 @@ namespace GAME_OFF_2020.GameStates
 
         public override void Draw(GameTimer gameTimer)
         {
+            BackgroundStars.DrawAll(BackgroundCamera.Position.ToVector2I(), 2);
             MapRenderer.DrawLayers(0, 1, Camera);
 
             SpriteBatch.Begin(SamplerType.Point, Camera.GetViewMatrix());
@@ -92,6 +125,7 @@ namespace GAME_OFF_2020.GameStates
             SpriteBatch.Begin(SamplerType.Point);
             SpriteBatch.DrawText(DefaultFont, Player.Position.ToVector2I().ToString(), new Vector2(25, 25), Veldrid.RgbaByte.White, 20, 1);
             SpriteBatch.DrawText(DefaultFont, Camera.ToString(), new Vector2(25, 50), Veldrid.RgbaByte.White, 20, 1);
+            SpriteBatch.DrawText(DefaultFont, BackgroundCamera.ToString(), new Vector2(25, 75), Veldrid.RgbaByte.White, 20, 1);
             SpriteBatch.End();
         }
     }
